62ece7a55234f26d60c4896f23f87ed64dc945b8
NadiiaKov <webking.dev3@gmail.com>
2017-01-11 11:15:19 +0200

    marctheme

---
+++ wp-content/themes/marctheme/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package marctheme
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'marctheme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'marctheme' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'marctheme' ), 'marctheme', '<a href="https://automattic.com/" rel="designer">webking.studio</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
