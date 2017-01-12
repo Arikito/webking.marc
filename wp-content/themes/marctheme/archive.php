f2c60e6bb13a3f89000ec1260dbe101eba7db550
NadiiaKov <webking.dev3@gmail.com>
2017-01-12 15:24:11 +0200

    Блог
    Проверка на наличие отрывка записи при выводе записей блога

--- wp-content/themes/marctheme/archive.php
+++ wp-content/themes/marctheme/archive.php
@@ -50,8 +50,7 @@ get_header(); ?>
 								</div>
 								<div class="col-md-6 col-lg-5">
 									<div class="blog-list__title"><?php	the_title( '<a href="' . esc_url( get_permalink() ) . '" class="updates-list__title" rel="bookmark">', '</a>' );?></div>
-									<div class="blog-list__description">
-									<?php do_excerpt(get_the_excerpt(), 100); ?></div>
+									<div class="blog-list__description"><?php if(has_excerpt()){ do_excerpt(get_the_excerpt(), 100); }?></div>
 									<div class="blog-list__footer">
 										<div class="blog-list__date"><img class="blog-list__date-icon" src="<?php echo get_template_directory_uri(); ?>/img/clock-icon.png" alt=""><?php marctheme_posted_on(); ?></div>
 										<a href="<? echo get_permalink(); ?>"><button class="blog-list__read-more btn btn-primary">Read more <span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span></button></a>
