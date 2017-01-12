390c00ac75f8a602cbc3395456892ea95c026f69
Alexander <arikito.91@gmail.com>
2017-01-12 16:38:38 +0200

    videos page

--- wp-content/themes/marctheme/page-videos.php
+++ wp-content/themes/marctheme/page-videos.php
@@ -9,38 +9,39 @@
 
 get_header(); ?>
 
+	<?php
+	$args = array(
+	    'post_type'=> 'post',
+	    'post_status' => 'publish',
+	    'order' => 'DESC',
+	    'tax_query' => array(
+	        array(
+	            'taxonomy' => 'post_format',
+	            'field' => 'slug',
+	            'terms' => array( 'post-format-video' )
+	        )
+	    )
+	);
+	$myposts = new WP_query($args);
+	?>
+
+	<section class="image-page-header image-page-header_videos container-fluid">
+		<?php the_title( '<h1 class="image-page-header__title">', ' <span class="image-page-header__quantity"> ('.wp_count_terms( 'post_format', 'post-format-video' ).')</span></h1>' )?>
+		<p class="image-page-header__subtitle"><?php echo get_post_meta(get_the_ID(), 'page_description', true); ?></p>
+	</section>
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-	<?php
-	?>
-	<section class="image-page-header image-page-header_videos container-fluid">
-		<?php the_title( '<h1 class="image-page-header__title">', ' <span class="image-page-header__quantity">()</span></h1>' )?> </h1>
-		<?php the_description( '<p class="image-page-header__subtitle">', '</p>' );?>
-	</section>
-	<?php if ( have_posts() ) :?>
-
-			<?php
-			$count = wp_count_posts('videos');
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
+			<?php if ( $myposts->have_posts() ) :
+				while ( $myposts->have_posts() ) : $myposts->the_post();
+					get_template_part( 'template-parts/content', 'video' );
+				endwhile;
+				$myposts->the_posts_navigation();
+			else :
+				$myposts->get_template_part( 'template-parts/content', 'none' );
+			endif;
+			wp_reset_postdata();
+			?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
