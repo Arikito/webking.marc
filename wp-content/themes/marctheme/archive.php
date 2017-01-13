9f15fc239d9a18d8bdd18ada9c4bce486547ff18
NadiiaKov <webking.dev3@gmail.com>
2017-01-13 15:40:38 +0200

    Пагинация в блоге

--- wp-content/themes/marctheme/archive.php
+++ wp-content/themes/marctheme/archive.php
@@ -39,7 +39,20 @@ get_header(); ?>
 						endwhile;
 
 						// the_posts_navigation();
-						echo paginate_links(array('format' => '?paged=%#%', 'show_all' => false, 'prev_next' => true));
+						// echo paginate_links(array('format' => '?paged=%#%', 'show_all' => false, 'prev_next' => true));
+						echo get_the_posts_pagination(
+							array(
+								'show_all'     => false, // показаны все страницы участвующие в пагинации
+								'end_size'     => 1,     // количество страниц на концах
+								'mid_size'     => 1,     // количество страниц вокруг текущей
+								'prev_next'    => true,  // выводить ли боковые ссылки "предыдущая/следующая страница".
+								'prev_text'    => html_entity_decode('<span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>'), //__('Previous'),
+								'next_text'    => html_entity_decode('<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>'), //__('Next'),
+								'add_args'     => false, // Массив аргументов (переменных запроса), которые нужно добавить к ссылкам.
+								'add_fragment' => '',     // Текст который добавиться ко всем ссылкам.
+								'screen_reader_text' => __( 'Posts navigation' ),
+							)
+						);
 
 					else :
 
